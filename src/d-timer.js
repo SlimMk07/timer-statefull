fa896d2aed4e7045c79e7b3ab78c47930284733b
unknown <satw7s@gmail.com>
2019-05-22 11:55:44 +0100

    2commit

--- src/d-timer.js
+++ src/d-timer.js
@@ -9,20 +9,22 @@ export default class Dynamic extends React.Component{
             h: Math.floor(props.time/3600000),
             m: Math.floor((props.time/1000%3600)/60),
             s: Math.floor((props.time/1000)%60),
+            ms: Math.floor((props.time%1000)),
             start: false
         };
     }
     startTimer=()=> {
         this.timer = setInterval(
             () => {
-              let time = parseInt(this.state.s)+parseInt(this.state.m*60)+parseInt(this.state.h*3600)+1
+              let time =parseInt(this.state.ms)+ parseInt(this.state.s*1000)+parseInt(this.state.m*60000)+parseInt(this.state.h*3600000)+5
               this.setState({
-                h: Math.floor(time/3600),
-                m: Math.floor((time%3600)/60),
-                s: Math.floor(time%60),
+                h: Math.floor(time/3600000),
+                m: Math.floor(((time/1000)%3600)/60),
+                s: Math.floor((time/1000)%60),
+                ms: Math.floor((time%1000)),
               })
             },
-            1000
+            5
         )
         this.setState({
             start: true
@@ -39,6 +41,7 @@ export default class Dynamic extends React.Component{
             h: Math.floor(this.props.time/3600000),
             m: Math.floor((this.props.time/1000%3600)/60),
             s: Math.floor((this.props.time/1000)%60),
+            ms: Math.floor((this.props.time%1000)),
             start: false
         })
         clearInterval(this.timer)
@@ -53,11 +56,14 @@ export default class Dynamic extends React.Component{
               <p className="time m">{("00" + this.state.m).slice(-2)}</p>
               <p className="time">:</p>
               <p className="time s">{("00" + this.state.s).slice(-2)}</p>
+              <p className="time">:</p>
+              <p className="time ms">{("000" + this.state.ms).slice(-3)}</p>
             </div>
             <div className='time-l'>
                <p className='heure'> Hour </p>
                <p className='minute'> Minute </p>
                <p className='seconde'> second </p>
+               <p className='m-seconde'> m-second </p>
             </div>
             <div className="butt">
                 <Button id="start" color="success" className="frst" onClick={this.state.start?this.stopTimer:this.startTimer}>{this.state.start?'Stop':'Start'}</Button>{' '}
